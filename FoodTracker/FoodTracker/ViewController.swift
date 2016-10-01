f18275c52e2a3961650e410aa72b2e2788bf03b9
Sigurdur E. Thorvaldsson <sigurdur@bu.edu>
2016-10-01 16:54:48 -0400

    trying to push FoodTracker to cloud

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Sigurdur Thorvaldsson on 01/10/16.
+//  Copyright © 2016 Sigurdur Thorvaldsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
